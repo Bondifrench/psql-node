4dc3758b2659d41b1eec07aa28863b7912f7ec2e
Bondifrench <d_dumaine@yahoo.com>
2014-04-24 14:46:08 +1000

    added query creating temporary table, inserting data and then querying table

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ var pg = require('pg.js');
 
 var conString = "postgres://nodetest:Welcome1@localhost:5432/nodetest";
 
-
 pg.connect(conString, function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('SELECT NOW() as when', function(err, result) {
-
-    if(err) {
-      return console.error('error running query', err);
-    }
-
-    console.log("Row count: %d",result.rows.length);
-    console.log("Current year: %d",result.rows[0].when.getFullYear());
-    //output: 1
-
-    //call `done()` to release the client back to the pool
-    done();
-  });
+  client.query("CREATE TEMP TABLE reviews(id SERIAL, author VARCHAR(50), content TEXT)");
+  client.query("INSERT INTO reviews(author, content) VALUES($1, $2)",
+    ["mad_reviewer", "I'd buy this any day of the week!!11"]);
+  client.query("INSERT INTO reviews(author, content) VALUES($1, $2)",
+    ["calm_reviewer", "Yes, that was a pretty good product."]);
+  client.query("SELECT * FROM reviews", function(err, result) {
+  console.log("Row count: %d",result.rows.length);  // 1
+  for (var i = 0; i < result.rows.length; i++) {
+    var row = result.rows[i];
+    console.log("id: " + row.id);
+    console.log("author: " + row.author);
+    console.log("content: " + row.content);
+  }
+});
+  //call `done()` to release the client back to the pool
+  done();
 });
